ebb3ab3cb47cd87c1f884fdcf86d6442cabf503d
Beavis <char440j@zbc.dk>
2024-08-19 10:11:13 +0200

    InsertionSort er blevet kodet

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,30 +1,20 @@
 import random, tests
 
-def bogoSort(items):
-    # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
+
+def insertionSort(items):
     items = items.copy()
-    isSorted = None # Boolean til markering af, om listen er sorteret
-    attempts = 0 # Tællevariabel til at holde styr på antal af forsøg
-    while not isSorted:
-        attempts += 1
-        if attempts > len(items) * 5000: # Check for at stoppe tendensen mod uendeligt
-            print('Giver op på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
-            items.sort()
-            return items
-        random.shuffle(items) # Bland alle elementer helt tilfældigt
-        isSorted = True # Vi går ud fra at listen tilfældigvis er sorteret,
-        # ...og prøver i denne løkke at bevise det modsatte
-        for index in range(len(items)-1):
-            if items[index] > items[index+1]:
-                isSorted = False
-                break # Bryd løkken hvis et eneste element er forkert sorteret
-    print('Sorteret efter {} forsøg'.format(attempts))
+    for x in range(0, len(items)):
+        for y in range(0, len(items)):
+            if y < x:
+                if items[x] < items[y]:
+                    items[x], items[y] = items[y], items[x]
     return items
+
 
 
 if __name__ == '__main__':
     ## Skriv navnet på den algoritme, der skal testes
-    algorithm = bogoSort
+    algorithm = insertionSort
 
     passedTest = True
     for i in range(10):
@@ -43,3 +33,7 @@ if __name__ == '__main__':
 
     print('blandet: ', lb)
     print('sorteret:', algorithm(ls))
+
+
+
+
